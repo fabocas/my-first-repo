6346a8a157708f67f2ef02b57283d3a34dd7f010
fsilva-p <fabio_bastos03@hotmail.com>
2026-03-13 12:58:07 +0100

    new solution

--- cyber-assessment/2-print_comb2.py
+++ cyber-assessment/2-print_comb2.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 
 for number in range(100):
-	if (number < 99):
-		print(f"{number}, ", end="")
-	else:
-		print(number)
-print('\n')
+	print("{:02d}{}".format(number, ", " if number < 99 else "\n"), end="")
